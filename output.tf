ca428d57e23e9de5f626a7766d106440df0552b7
Mijaíl Rondón <mijail@riseup.net>
2025-06-05 14:55:25 -0500

    fix: trigger tag creation

--- output.tf
+++ output.tf
@@ -4,6 +4,5 @@ output "byoc_setup_details" {
     project_id        = var.project_id
     bucket_name       = google_storage_bucket.terraform_state.name
     workload_provider = google_iam_workload_identity_pool_provider.github_provider.name
-
   }
 }
